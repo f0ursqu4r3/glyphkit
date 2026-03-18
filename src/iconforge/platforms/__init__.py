b43486cde27075f353d48ee87a2af51039f125af
kyle dougan <kyle.dougan@e-o.solutions>
2026-03-18 10:50:00 -1000

    feat: Windows .ico generator

--- src/iconforge/platforms/__init__.py
+++ src/iconforge/platforms/__init__.py
@@ -5,9 +5,11 @@ from __future__ import annotations
 from iconforge.platforms.ios import generate as ios_generate
 from iconforge.platforms.android import generate as android_generate
 from iconforge.platforms.macos import generate as macos_generate
+from iconforge.platforms.windows import generate as windows_generate
 
 PLATFORM_REGISTRY: dict[str, object] = {
     "ios": ios_generate,
     "android": android_generate,
     "macos": macos_generate,
+    "windows": windows_generate,
 }
